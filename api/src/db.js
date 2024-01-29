033279c75c0fbf409690a676e34da5b4e2fa0bb7
Diegoc24 <carusodiegod@gmail.com>
2024-01-29 01:52:03 -0300

    feature/backend(ModificationDb)

--- api/src/db.js
+++ api/src/db.js
@@ -6,9 +6,21 @@ const {
     DB_USER,
     DB_PASSWORD,
     DB_HOST,
-    DB_PORT} = process.env
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,{
+    DB_PORT,
+    DEPLOYING,
+    DB_URI} = process.env
+
+let sequelize
+    if(DEPLOYING === "false"){
+        sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,{
+    logging: false,
+    native: false
+})
+    }else if(DEPLOYING === "true"){
+        sequelize = new Sequelize(`${DB_URI}`,{
     logging: false
 })
+    }
+
 
 module.exports = sequelize;
