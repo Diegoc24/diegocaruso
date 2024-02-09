f092edff45a8e8e6edae65b3bdac5cefd66cc955
Diegoc24 <carusodiegod@gmail.com>
2024-02-09 16:39:55 -0300

    feature/backend(fixv1)

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Footer from './components/Footer/Footer';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = `${REACT_APP_API_URL}`;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = `*`;
 function App() {
   console.log(process.env.REACT_APP_API_URL)
   return (
