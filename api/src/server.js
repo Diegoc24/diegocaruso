e1e8b4a15ed43fefaf0e6a579a3d81c67797faf3
Diegoc24 <carusodiegod@gmail.com>
2024-02-09 16:52:49 -0300

    feature/backend(fixv1)

--- api/src/server.js
+++ api/src/server.js
@@ -7,15 +7,11 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(morgan("dev"))
 server.use(cors({
-    origin: "*"
-}))
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+  origin: "*",
+  credentials: true,
+  methods: "GET, POST, OPTIONS, PUT, DELETE",
+  headers: "Origin, X-Requested-With, Content-Type, Accept"
+}));
 server.use("/", router)
 
 module.exports = server;
